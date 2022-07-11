ff908c342e6a94a28a69cace665b7e8daece9acb
Cyrus Bigham <bighamcyrus@gmail.com>
2022-07-10 18:50:43 -0700

    added bootstrap and password generator to work section

---
+++ index.js
@@ -0,0 +1,32 @@
+// Conlog number loop 
+
+// let number = 0;
+// while (number <= 70)
+// {
+//     console.log(number);
+//     number = number + 7;
+// }
+// You could also write the same expression it like this
+// for(let number = 0; number <= 12; number = number + 2) 
+// {
+//     console.log(number);
+// }
+
+// Create a promt for a username and passcode
+// prompt ("Enter Passcode");
+// console.log (prompt)
+
+
+//  FIZZBUZZ/ ELSE IF  PRACTICE
+function fizzBuzz(number)
+{
+    for (let i = 0; i <= number; i++) {
+        if(i % 3 === 0 && i % 5 === 0) console.log("FizzBuzz");
+        else if (i % 3 === 0) console.log("Fizz");
+        else if (i % 5 === 0) console.log("Buzz");
+        else console.log(i);
+    }
+
+}
+
+fizzBuzz(100);
