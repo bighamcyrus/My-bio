0a47f538dfc8e3cfa60d789c26bf58ac9e35570e
Cyrus Bigham <bighamcyrus@gmail.com>
2022-07-23 18:58:43 -0700

    updated css stylesheets and page layout

--- dist/index.js
+++ dist/index.js
@@ -18,15 +18,15 @@
 
 
 //  FIZZBUZZ/ ELSE IF  PRACTICE
-function fizzBuzz(number)
-{
-    for (let i = 0; i <= number; i++) {
-        if(i % 3 === 0 && i % 5 === 0) console.log("FizzBuzz");
-        else if (i % 3 === 0) console.log("Fizz");
-        else if (i % 5 === 0) console.log("Buzz");
-        else console.log(i);
-    }
+// function fizzBuzz(number)
+// {
+//     for (let i = 0; i <= number; i++) {
+//         if(i % 3 === 0 && i % 5 === 0) console.log("FizzBuzz");
+//         else if (i % 3 === 0) console.log("Fizz");
+//         else if (i % 5 === 0) console.log("Buzz");
+//         else console.log(i);
+//     }
 
-}
+// }
 
-fizzBuzz(100);
+// fizzBuzz(100);
